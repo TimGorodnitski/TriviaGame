d8dbeb5b4d43c25ec747901074e6f84c4ddaa684
TimG <timgm3@gmail.com>
2018-08-29 18:55:44 -0700

    question advancement and stat tracking working!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,11 @@
+let correct = 0;
+let incorrect = 0;
+let unanswered = 0;
+
+
+
+
+
 var questions = [
     {   // question 1
         question: "What color is the sky?",
@@ -7,7 +15,6 @@ var questions = [
             "Pink",
             "Red"],
         correctAnswer: "Blue"
-
     }, { // question 2
         question: "Who is our President?",
         answers: [
@@ -16,7 +23,6 @@ var questions = [
             "Pink",
             "Red"],
         correctAnswer: "Trump"
-
     }, { // question 3
         question: "This answer is D",
         answers: [
@@ -25,7 +31,83 @@ var questions = [
             "C",
             "D"],
         correctAnswer: "D"
-    }];
+    }
+
+
+    // }, { // question 4
+    //     question: "Question?",
+    //     answers: [
+    //         "Answer1",
+    //         "Answer2",
+    //         "Answer3",
+    //         "Answer4"],
+    //     correctAnswer: "Answer4"
+    // }, { // question 5
+    //     question: "Question?",
+    //     answers: [
+    //         "Answer1",
+    //         "Answer2",
+    //         "Answer3",
+    //         "Answer4"],
+    //     correctAnswer: "Answer4"
+    // }, { // question 6
+    //     question: "Question?",
+    //     answers: [
+    //         "Answer1",
+    //         "Answer2",
+    //         "Answer3",
+    //         "Answer4"],
+    //     correctAnswer: "Answer4"
+    // }, { // question 7
+    //     question: "Question?",
+    //     answers: [
+    //         "Answer1",
+    //         "Answer2",
+    //         "Answer3",
+    //         "Answer4"],
+    //     correctAnswer: "Answer4"
+    // }, { // question 8
+    //     question: "Question?",
+    //     answers: [
+    //         "Answer1",
+    //         "Answer2",
+    //         "Answer3",
+    //         "Answer4"],
+    //     correctAnswer: "Answer4"
+    // }, { // question 9
+    //     question: "Question?",
+    //     answers: [
+    //         "Answer1",
+    //         "Answer2",
+    //         "Answer3",
+    //         "Answer4"],
+    //     correctAnswer: "Answer4"
+    // }, { // question 10
+    //     question: "Question?",
+    //     answers: [
+    //         "Answer1",
+    //         "Answer2",
+    //         "Answer3",
+    //         "Answer4"],
+    //     correctAnswer: "Answer4"
+    // }, { // question 11
+    //     question: "Question?",
+    //     answers: [
+    //         "Answer1",
+    //         "Answer2",
+    //         "Answer3",
+    //         "Answer4"],
+    //     correctAnswer: "Answer4"
+    // }, { // question 12
+    //     question: "Question?",
+    //     answers: [
+    //         "Answer1",
+    //         "Answer2",
+    //         "Answer3",
+    //         "Answer4"],
+    //     correctAnswer: "Answer4"
+    // }
+];
 
 
 
@@ -35,17 +117,33 @@ $("#startButton").click(function () {
     questionStart();
 })
 
-function questionStart(){
-    let i = 0;
-    $("#question span").text(questions[i].question);
-    $("#choice0 span").text(questions[i].answers[0]);
-    $("#choice1 span").text(questions[i].answers[1]);
-    $("#choice2 span").text(questions[i].answers[2]);
-    $("#choice3 span").text(questions[i].answers[3]);
-    i++;
-    console.log(i)
+let i = 0;
+
+function questionStart() {
+    if (i < questions.length) {
+        $("#question span").text(questions[i].question);
+        $("#choice0 span").text(questions[i].answers[0]);
+        $("#choice1 span").text(questions[i].answers[1]);
+        $("#choice2 span").text(questions[i].answers[2]);
+        $("#choice3 span").text(questions[i].answers[3]);
+    } else {
+        $("#gameDiv").hide();
+    }
 };
 
+$(".choice").click(function questionAnswer() {
 
+    if ($(this).find("span").text() === questions[i].correctAnswer) {
 
+        correct++;
+
+    } else {
+
+        incorrect++;
+    };
+
+    i++;
+    questionStart();
+
+})
 
