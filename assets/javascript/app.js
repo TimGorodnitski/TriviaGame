6a164e23a5519045ee0c23174f9d97439204f5d2
TimG <timgm3@gmail.com>
2018-08-29 18:04:51 -0700

    answer array created, question and answers being pushed to page divs properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,50 @@
-questions = {
+var questions = [
+    {   // question 1
+        question: "What color is the sky?",
+        answers: [
+            "Blue",
+            "Green",
+            "Pink",
+            "Red"],
+        correctAnswer: "Blue"
 
-    q1: function () {
-        $("#question").append("The answer is C");
-    }
+    }, { // question 2
+        question: "Who is our President?",
+        answers: [
+            "Blue",
+            "Trump",
+            "Pink",
+            "Red"],
+        correctAnswer: "Trump"
 
-}
+    }, { // question 3
+        question: "This answer is D",
+        answers: [
+            "A",
+            "B",
+            "C",
+            "D"],
+        correctAnswer: "D"
+    }];
 
-
-
-
-
-
-
-
-$("#gameDiv").hide();
 
 
 $("#startButton").click(function () {
     $("#start").hide();
     $("#gameDiv").show();
-    questions.q1();
+    questionStart();
 })
 
+function questionStart(){
+    let i = 0;
+    $("#question span").text(questions[i].question);
+    $("#choice0 span").text(questions[i].answers[0]);
+    $("#choice1 span").text(questions[i].answers[1]);
+    $("#choice2 span").text(questions[i].answers[2]);
+    $("#choice3 span").text(questions[i].answers[3]);
+    i++;
+    console.log(i)
+};
 
 
 
